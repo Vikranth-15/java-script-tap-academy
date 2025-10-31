90d39498a8d742dbc9753340598045b7c52fbd65
vikranth <vikranthyadlapalli123@gmail.com>
2025-10-31 10:55:16 +0530

    Number to string

---
+++ Typecasting.js
@@ -0,0 +1,13 @@
+let a=123
+console.log(a);
+console.log(typeof(a));
+let b=String(a);
+console.log(b);
+console.log(typeof(b));
+
+
+// output-->
+//     123
+//     number
+//     123
+//     string

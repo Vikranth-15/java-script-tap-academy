19f55f4074eaf84c0941e3d347f2cfd59cf5387f
vikranth <vikranthyadlapalli123@gmail.com>
2025-10-31 14:18:14 +0530

    conditional statement

---
+++ IF-ElSE.js
@@ -0,0 +1,28 @@
+let age =18;
+if(age>20){
+    console.log("Welcome to the club");
+}
+else{
+    console.log("You have to be in waiting list");
+}
+
+
+// output---->
+//         ou have to be in waiting list
+
+
+let balance =10000;
+
+let withDrawAmt=8000;
+
+if(balance>=withDrawAmt){
+    balance-=withDrawAmt;
+    console.log("Transaction Successful");
+}
+else{
+    console.log("Insufficient Balance");
+}
+
+
+// output ----->
+//         Transaction Successful

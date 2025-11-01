127cc0b3d11d2b78ce76ff025b835964d4fdd14f
vikranth <vikranthyadlapalli123@gmail.com>
2025-11-01 15:35:29 +0530

    Hoisting in javascript

---
+++ Hoisting.js
@@ -0,0 +1,52 @@
+
+
+
+let a;  // declaration
+
+a=100;  // Initialization 
+
+console.log(a);   // 100 
+
+
+
+//Hoisting --->  The process of moving declarations to the top of program 
+
+// console.log(b);
+// let b=200;          
+
+//  gives error Reference Error 
+
+
+// Var Keyword 
+
+//When you declare a variable not assigned any value it stored as default value 
+
+let c;
+console.log(c);    // output----> undefined 
+
+var cc;
+console.log(cc);    // output----> undefined 
+
+
+
+console.log(b);
+var b=100;      // output ---->  Undefined 
+
+
+// Var key word Hosisting 
+
+
+console.log(c1);     // Output ----> undefined
+var c1=100;
+console.log(c1);     //  Output ----> 100
+
+
+// let keyword 
+
+console.log(c2);     // Output ----> Reference Error  not initialized
+let c2=100;
+console.log(c2);     //  Output ----> Reference Error 
+
+
+
+

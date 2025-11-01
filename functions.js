8d6ffbb08662bea814d68196f67811cbdcd6a3c0
vikranth <vikranthyadlapalli123@gmail.com>
2025-11-01 11:49:41 +0530

    functions in javaSCript

--- functions.js
+++ functions.js
@@ -13,4 +13,36 @@ function add(){
 add();
 
 
+let aa=(x,y) =>{
+    return x+y;
+}
+
+let sum=(x,y) => x+y;
+// arrow functions
+
+
+console.log(aa(1000,2000));
+
+console.log(sum(10,20));
+
+
+//Immediately Invoking Function Expression
+(function(){
+    console.log("Immediately Invoked Function Expression");
+})();
+
+
+
+
+//Function Expression
+
+let sub=function(x,y){
+    return x-y;
+}
+
+let subb=sub(2000,1000);
+console.log(subb);  //output ---> 1000
+
+
+
 

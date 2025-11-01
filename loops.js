945b0b70d2d7d769237350979c75fbf9e7d00e1d
vikranth <vikranthyadlapalli123@gmail.com>
2025-11-01 09:53:25 +0530

    loops in js

---
+++ loops.js
@@ -0,0 +1,41 @@
+for(let i=1;i<6;i++){
+    console.log("* ");
+}
+
+// output---->
+//             * 
+//             * 
+//             * 
+//             * 
+//             * 
+
+
+
+let a=1;
+while(a<=5){
+    console.log("* ");
+    a++;
+}
+
+// output---->
+//         * 
+//         * 
+//         * 
+//         * 
+//         * 
+
+
+let z=1;
+do{
+    console.log("* ");
+    z++;
+}while(z<=5)
+
+
+// output----->
+
+//             * 
+//             * 
+//             * 
+//             * 
+//             * `

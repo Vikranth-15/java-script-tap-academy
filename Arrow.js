1a060cd21dd6c6de416ca25e763424bfd55a375b
Vikranth <vikranthyadlapalli123@gmail.com>
2026-02-21 09:49:50 +0530

    Arrow functions

---
+++ Arrow.js
@@ -0,0 +1,22 @@
+//Function Declaration
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(3,4));
+
+
+//Function Expression
+let summ=function(a,b){
+    return a+b;
+}
+
+console.log(summ(7,8));
+
+
+
+let sum1=(a,b) =>{
+    return a+b;
+}
+
+console.log(sum1(100,200))

a1fd49bc9eaf077ffafd27d4c9ebcf41f7658428
Vikranth <vikranthyadlapalli123@gmail.com>
2026-02-21 09:48:52 +0530

    Ternary Operator

---
+++ TernaryOperator.js
@@ -0,0 +1,8 @@
+let speed=101;
+let message=speed>100? "Too Fast" :"Normal Speed";
+console.log(message);  //Too Fast
+
+
+// let speed=92;
+// let message=speed>100? "Too Fast" :"Normal Speed";
+// console.log(message);  //Normal Speed
